7bd53a98caeff6b7b3ff9ef5ccfe9e9ebc83ee82
bottleofench <71398923+bottleofench@users.noreply.github.com>
2022-08-23 16:01:14 +0600

    Add BowBuilder

---
+++ src/main/java/api/bottleofench/fruitfulapi/builders/itemstack/BowBuilder.java
@@ -0,0 +1,32 @@
+package api.bottleofench.fruitfulapi.builders.itemstack;
+
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityShootBowEvent;
+import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.ProjectileLaunchEvent;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Consumer;
+
+public class BowBuilder extends ItemStackBuilder implements Listener {
+    protected List<Consumer<EntityShootBowEvent>> bowShootHandlers = new ArrayList<>();
+
+    public BowBuilder() {
+        super(Material.BOW);
+    }
+
+    public BowBuilder addBowShootHandler(Consumer<EntityShootBowEvent>... consumers) {
+        this.bowShootHandlers.addAll(List.of(consumers));
+        return this;
+    }
+
+    @EventHandler
+    private void onShoot(EntityShootBowEvent event) {
+        if (!Objects.equals(event.getBow(), item)) return;
+        this.bowShootHandlers.forEach(entityShootBowHandler -> entityShootBowHandler.accept(event));
+    }
+}
