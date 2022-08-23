531ef6bb57d2585813a97c274a804c6652d6dacb
bottleofench <71398923+bottleofench@users.noreply.github.com>
2022-08-23 16:30:05 +0600

    Add WeaponBuilder

---
+++ src/main/java/api/bottleofench/fruitfulapi/builders/itemstack/WeaponBuilder.java
@@ -0,0 +1,30 @@
+package api.bottleofench.fruitfulapi.builders.itemstack;
+
+import com.destroystokyo.paper.MaterialTags;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class WeaponBuilder extends ItemStackBuilder implements Listener {
+    protected List<Consumer<EntityDamageByEntityEvent>> attackHandlers = new ArrayList<>();
+
+    public WeaponBuilder(Material material) {
+        super(MaterialTags.AXES.isTagged(material) || MaterialTags.SWORDS.isTagged(material)
+                ? material : Material.STONE_SWORD);
+    }
+
+    public WeaponBuilder addAttackHandler(Consumer<EntityDamageByEntityEvent>... consumers) {
+        this.attackHandlers.addAll(List.of(consumers));
+        return this;
+    }
+
+    @EventHandler
+    private void onAttack(EntityDamageByEntityEvent event) {
+        this.attackHandlers.forEach(attackHandler -> attackHandler.accept(event));
+    }
+}
